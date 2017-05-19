0a918c2ea4cfb0288e7aa4c8302cc74b673effb8
Stephen Tyree <styree@nvidia.com>
2017-05-19 10:44:31 -0500

    input size

--- model.py
+++ model.py
@@ -34,7 +34,7 @@ class ES(torch.nn.Module):
             self.bnconv1 = nn.BatchNorm2d(16, affine=False, momentum=1.)
             self.conv2 = nn.Conv2d(16, 32, 4, stride=2)
             self.bnconv2 = nn.BatchNorm2d(32, affine=False, momentum=1.)
-            n_conv_outputs = self.conv2(self.conv1(
+            self.num_conv_outputs = self.conv2(self.conv1(
                     torchify(np.zeros(observation_space.shape)))).numel()
         else:
             self.conv1 = nn.Conv2d(num_inputs, 32, 3, stride=2, padding=1)
@@ -45,11 +45,11 @@ class ES(torch.nn.Module):
             self.bnconv3 = nn.BatchNorm2d(32, affine=False, momentum=1.)
             self.conv4 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
             self.bnconv4 = nn.BatchNorm2d(32, affine=False, momentum=1.)
-            n_conv_outputs = self.conv4(self.conv3(self.conv2(self.conv1(
+            self.num_conv_outputs = self.conv4(self.conv3(self.conv2(self.conv1(
                     torchify(np.zeros(observation_space.shape)))))).numel()
         
         # fc layers
-        self.fc1 = nn.Linear(n_conv_outputs, 256)
+        self.fc1 = nn.Linear(self.num_conv_outputs, 256)
         self.bnfc1 = nn.BatchNorm1d(256, affine=False, momentum=1.)
         self.fc2 = nn.Linear(256, num_outputs)
         self.bnfc2 = nn.BatchNorm1d(num_outputs, affine=False, momentum=1.)
@@ -67,17 +67,14 @@ class ES(torch.nn.Module):
         if self.a3c_net:
             x = F.relu(self.bnconv1(self.conv1(inputs)))
             x = F.relu(self.bnconv2(self.conv2(x)))
-            x = x.view(-1, 32*3*3)
-            x = F.relu(self.bnfc1(self.fc1(x)))
-            return F.softmax(self.bnfc2(self.fc2(x)))
         else:
             x = F.relu(self.bnconv1(self.conv1(inputs)))
             x = F.relu(self.bnconv2(self.conv2(x)))
             x = F.relu(self.bnconv3(self.conv3(x)))
             x = F.relu(self.bnconv4(self.conv4(x)))
-            x = x.view(-1, 32*3*3)
-            x = F.relu(self.bnfc1(self.fc1(x)))
-            return F.softmax(self.bnfc2(self.fc2(x)))
+        x = x.view(-1, self.num_conv_outputs)
+        x = F.relu(self.bnfc1(self.fc1(x)))
+        return F.softmax(self.bnfc2(self.fc2(x)))
                 
     def do_virtual_batch_norm(self, batch):
         if not self.use_virtual_batch_norm:
